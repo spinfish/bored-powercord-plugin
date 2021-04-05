7aafeb5d78cd7326c0d43c6ecf90497482f4fe86
mackenzie <2005nenalinda@gmail.com>
2021-04-05 14:12:43 +1200

    update permissions, license, and general code overhaul

--- index.js
+++ index.js
@@ -1,74 +1,78 @@
 const { get } = require('powercord/http');
 const { Plugin } = require('powercord/entities');
 
+const boredApiUrl = 'http://www.boredapi.com/api/activity/';
+
 module.exports = class Bored extends Plugin {
+  startPlugin() {
+    powercord.api.commands.registerCommand({
+      command: 'bored',
+      description: 'Bust your boredom... hopefully!',
+      usage: '{c}',
+      executor: this.search.bind(this)
+    });
+  }
 
-    startPlugin() {
-        powercord.api.commands.registerCommand({
-            command: 'bored',
-            description: 'Bust your boredom (hopefully) !',
-            usage: '{c}',
-            executor: this.search.bind(this)
-        })
+  pluginWillUnload() {
+    powercord.api.commands.unregisterCommand('bored');
+  }
+
+  get getRandomColour() {
+    return Math.floor(Math.random() * 16777215);
+  }
+
+  doCapitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  async search() {
+    const fallback = {
+      send: false,
+      result: 'Something broke with the API :('
+    };
+
+    try {
+      const jsonBody = await get(boredApiUrl).then(resp => resp.body);
+    } catch (error) {
+        console.error('Error occurred in Bored plugin: ', error);
+        return fallback;
+      }
+
+    if (!jsonBody.activity) {
+      return fallback;
     }
 
-    pluginWillUnload() {
-        powercord.api.commands.unregisterCommand('bored');
-    }
+    let information = [];
 
-    capitalize(s) {
-        if (typeof(s) !== 'string') {
-            s = s.toString();
+    Object.entries(jsonBody).forEach((arrayItem) => {
+      const [ key, value ] = arrayItem;
+      const keyCapitalized = `• ${capitalize(key)}: `;
+      let valueCapitalized; // gotta do this otherwise reference errors
+
+      if (key === 'link' && value) {
+        valueCapitalized = `[Click Here](${value})`;
+      } else {
+          valueCapitalized = capitalize(value || 'none');
+      }
+      information.push(keyCapitalized + valueCapitalized);
+    });
+
+    return {
+      send: false,
+      result: {
+        type: 'rich',
+        title: 'Feeling bored? How about you...',
+        description: `${jsonBody.activity}!`,
+        color: this.getRandomColour,
+        fields: [{
+          name: 'Extra information',
+          value: information.join('\n'),
+          inline: false
+        }],
+        footer: {
+          text: 'Sourced from https://www.boredapi.com/'
         }
-        return s.charAt(0).toUpperCase() + s.slice(1);
-    }
-
-    randomColourInteger() {
-        return Math.floor(Math.random() * 16777215);
-    }
-
-    async search() {
-        const url = 'http://www.boredapi.com/api/activity/';
-        const res = await get(url).then(r => r.body);
-
-        if (!res.activity) {
-            return {
-                send: false,
-                result: 'Something broke with the API :('
-            };
-        }
-
-        const activityDescription = res.activity;
-        delete res.activity;
-
-        let information = [];
-
-        for (let key in res) {
-            let item;
-            if (key === 'link' && res[key]) {
-                item = `• ${this.capitalize(key)}: [Click Here](${res[key]})`
-            } else {
-                item = `• ${this.capitalize(key)}: ${this.capitalize(res[key] || 'none')}`
-            }
-            information.push(item);
-        }
-
-        return {
-            send: false,
-            result: {
-                type: 'rich',
-                title: 'Feeling bored? How about you...',
-                description: `${activityDescription}!`,
-                color: this.randomColourInteger(),
-                fields: [{
-                    name: 'Extra information',
-                    value: information.join('\n'),
-                    inline: false
-                }],
-                footer: {
-                    text: 'Sourced from https://www.boredapi.com/'
-                }
-            }
-        };
-    }
+      }
+    };
+  }
 }
