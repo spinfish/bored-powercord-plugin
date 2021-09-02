bbbbc39d2407aaaed18bf272fac611e7ce37ce96
mackenzie <2005nenalinda@gmail.com>
2021-09-02 14:50:06 +1200

    Fix undefined jsonBody, overall code haul

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# bored
-a very simple powercord plugin which adds a ``bored`` command to give you ideas of things to do when bored.
+# Bored
+A very simple powercord plugin which adds a ``bored`` command to give you ideas of things to do when bored.
 
-## installation
-1. make sure you have powercord installed ([click here](https://powercord.dev/installation "hi there") to do that)
-2. go to your where your powercord folder is
-3. navigate to the ``src`` > ``Powercord`` > ``plugins`` folder
-4. right click this and select ``Git Bash Here`` (as shown in the screenshot below), and then type:
+## Installation
+1. Make sure you have powercord installed ([click here](https://powercord.dev/installation "hi there") to do that)
+2. Go to your where your powercord folder is
+3. Navigate to the ``src`` > ``Powercord`` > ``plugins`` folder
+4. Right click this and select ``Git Bash Here`` (as shown in the screenshot below), and then type:
 
 ```shell
 git clone https://github.com/spinfish/bored-powercord-plugin
@@ -13,8 +13,8 @@ git clone https://github.com/spinfish/bored-powercord-plugin
 
 ![image](https://media.discordapp.net/attachments/668999977833463821/784620760617451550/gitbashhere_.png?width=455&height=253)
 
-## license
+## License
 MIT, see [`LICENSE`](./LICENSE.md) for more details
 
-## examples
+## Examples
 ![image](https://media.discordapp.net/attachments/668999977833463821/784630038947037195/unknown.png)
--- index.js
+++ index.js
@@ -1,78 +1,92 @@
-const { get } = require('powercord/http');
-const { Plugin } = require('powercord/entities');
+const { get } = require("powercord/http");
+const { Plugin } = require("powercord/entities");
 
-const boredApiUrl = 'http://www.boredapi.com/api/activity/';
+const API_URL = "http://www.boredapi.com/api/activity/";
 
 module.exports = class Bored extends Plugin {
   startPlugin() {
     powercord.api.commands.registerCommand({
-      command: 'bored',
-      description: 'Bust your boredom... hopefully!',
-      usage: '{c}',
+      command: "bored",
+      description: "Bust your boredom... hopefully!",
+      usage: "{c}",
       executor: this.search.bind(this)
-    });
+    })
+    console.log("Hello")
   }
 
   pluginWillUnload() {
-    powercord.api.commands.unregisterCommand('bored');
+    powercord.api.commands.unregisterCommand("bored")
   }
 
-  get getRandomColour() {
-    return Math.floor(Math.random() * 16777215);
+  get randomColour() {
+    return Math.floor(Math.random() * 16777215)
   }
 
-  doCapitalize(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  capitalizeString(value) {
+    if (typeof value === "string") {
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    } else {
+      return value
+    }
   }
 
   async search() {
     const fallback = {
       send: false,
-      result: 'Something broke with the API :('
-    };
-
-    try {
-      const jsonBody = await get(boredApiUrl).then(resp => resp.body);
-    } catch (error) {
-        console.error('Error occurred in Bored plugin: ', error);
-        return fallback;
-      }
-
-    if (!jsonBody.activity) {
-      return fallback;
+      result: "Something broke with the API :("
     }
 
-    let information = [];
+    let jsonBody
+    try {
+      jsonBody = await get(API_URL).then(resp => resp.body)
+    } catch (error) {
+      console.error("Error occured in Bored plugin: ", error)
+      return fallback
+    }
 
-    Object.entries(jsonBody).forEach((arrayItem) => {
-      const [ key, value ] = arrayItem;
-      const keyCapitalized = `• ${capitalize(key)}: `;
-      let valueCapitalized; // gotta do this otherwise reference errors
+    const activity = jsonBody.activity
+    if (!activity) {
+      return fallback
+    }
 
-      if (key === 'link' && value) {
-        valueCapitalized = `[Click Here](${value})`;
-      } else {
-          valueCapitalized = capitalize(value || 'none');
+    delete jsonBody.activity
+    let information = []
+
+    Object.entries(jsonBody).forEach((arr) => {
+      const [ attr, value ] = arr
+
+      if (!(value === null || value.length === 0)) {
+        let finalValue
+        const finalAttr = `• ${this.capitalizeString(attr)}: `
+
+        if (attr === "link" && finalValue.startsWith("http")) {
+          finalValue = value // Lame
+        } else {
+          finalValue = this.capitalizeString(value)
+        }
+
+        information.push(finalAttr + finalValue)
       }
-      information.push(keyCapitalized + valueCapitalized);
-    });
+    })
 
     return {
       send: false,
       result: {
-        type: 'rich',
-        title: 'Feeling bored? How about you...',
-        description: `${jsonBody.activity}!`,
-        color: this.getRandomColour,
-        fields: [{
-          name: 'Extra information',
-          value: information.join('\n'),
-          inline: false
-        }],
+        type: "rich",
+        title: "Feeling bored? How about you...",
+        description: `${activity}!`,
+        color: this.randomColour,
+        fields: [
+          {
+            name: "Extra information",
+            value: information.join("\n"),
+            inline: false
+          }
+        ],
         footer: {
-          text: 'Sourced from https://www.boredapi.com/'
+          text: "Sourced from https://www.boredapi.com/"
         }
       }
-    };
+    }
   }
 }
